5f6fed3e9b397843e2265151138be1dcec6012e1
y0mar <68041642+y0mar@users.noreply.github.com>
2020-07-23 10:58:45 -0700

    included file chooser

---
+++ src/scrabblePackage/Rack.java
@@ -0,0 +1,14 @@
+package scrabblePackage;
+import java.util.*;
+import java.io.*;
+
+public class Rack {
+	String letterStr;
+	int letters[] = new int[26];
+	
+	
+	void loadRack(String str) {
+		letterStr = str;
+	}
+
+}
