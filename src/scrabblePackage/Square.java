2507d32f728189b76469772967d6cfb8754dbb0a
y0mar <68041642+y0mar@users.noreply.github.com>
2020-07-23 20:44:04 -0700

    Revert "included file chooser"
    This reverts commit 5f6fed3e9b397843e2265151138be1dcec6012e1.

--- src/scrabblePackage/Square.java
+++
@@ -1,9 +0,0 @@
-package scrabblePackage;
-import java.util.*;
-
-public class Square {
-	
-	int multiplier, row, col;
-	
-
-}
--- src/scrabblePackage/driver.java
+++ src/scrabblePackage/driver.java
@@ -1,26 +1,8 @@
 package scrabblePackage;
-import java.awt.FileDialog;
-import java.io.File;
-import java.io.IOException;
 import java.util.*;
 
-import javax.swing.JFrame;
-
 public class driver {
-	public static void main(String[] args) throws IOException {
-		JFrame frame = new JFrame();
-		FileDialog dialog = new FileDialog(frame, "Select a file", FileDialog.LOAD);
-		dialog.setDirectory("C://");
-		dialog.setFile("*.txt");
-		dialog.setVisible(true);
-		Scanner input = new Scanner(System.in);
-		
-		String filename = dialog.getFile();
-		File file = new File(dialog.getDirectory() + filename);
-		Scanner fileReader = new Scanner(file);
-		
-		
-		
+	public static void main(String[] args) {
 		
 	}
 
